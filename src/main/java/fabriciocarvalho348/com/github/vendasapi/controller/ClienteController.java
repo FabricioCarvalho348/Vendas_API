f141e71b09352f5fb8cb480087133c45e9598c9a
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2023-08-30 15:15:41 -0300

    feat: Adicionando listar por id, listar todos, atualizar por id e excluir por id | refactor: incluir cliente |

--- src/main/java/fabriciocarvalho348/com/github/vendasapi/controller/ClienteController.java
+++ src/main/java/fabriciocarvalho348/com/github/vendasapi/controller/ClienteController.java
@@ -3,10 +3,9 @@ package fabriciocarvalho348.com.github.vendasapi.controller;
 import fabriciocarvalho348.com.github.vendasapi.model.entity.Cliente;
 import fabriciocarvalho348.com.github.vendasapi.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -15,10 +14,32 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @PostMapping
-    public Cliente incluir(@RequestBody Cliente cliente) {
-        Cliente clienteResponse = clienteService.incluir(cliente);
-        return clienteResponse;
+    @GetMapping("/{id}")
+    public Cliente listarPorId(@PathVariable Long id) {
+        Cliente response = clienteService.pesquisarPorId(id);
+        return response;
     }
 
+    @GetMapping
+    public List<Cliente> pesquisarTodos() {
+        List<Cliente> response = clienteService.pesquisarTodos();
+        return response;
+    }
+
+    @PostMapping
+    public Cliente incluir(@RequestBody Cliente cliente) {
+        Cliente response = clienteService.incluir(cliente);
+        return response;
+    }
+
+    @PutMapping("/{id}")
+    public Cliente atualizar(@RequestBody Cliente cliente, @PathVariable Long id) {
+        Cliente response = clienteService.atualizar(cliente, id);
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        clienteService.excluir(id);
+    }
 }
--- src/main/java/fabriciocarvalho348/com/github/vendasapi/service/ClienteService.java
+++ src/main/java/fabriciocarvalho348/com/github/vendasapi/service/ClienteService.java
@@ -3,7 +3,11 @@ package fabriciocarvalho348.com.github.vendasapi.service;
 import fabriciocarvalho348.com.github.vendasapi.model.entity.Cliente;
 import fabriciocarvalho348.com.github.vendasapi.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
@@ -11,8 +15,33 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    public Cliente pesquisarPorId(Long id) {
+        Cliente encontrado = clienteRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+
+        return encontrado;
+    }
+
+    public List<Cliente> pesquisarTodos() {
+        List<Cliente> lista = clienteRepository.findAll();
+        return lista;
+    }
+
     public Cliente incluir(Cliente cliente) {
-        Cliente clienteSalvo = clienteRepository.save(cliente);
-        return clienteSalvo;
+        Cliente salvo = clienteRepository.save(cliente);
+        return salvo;
+    }
+
+    public Cliente atualizar(Cliente cliente, Long id) {
+        Cliente encontrado = pesquisarPorId(id);
+        cliente.setId(encontrado.getId());
+        Cliente salvo = clienteRepository.save(cliente);
+        return salvo;
+    }
+
+    public void excluir(Long id) {
+        Cliente encontrado = pesquisarPorId(id);
+
+        clienteRepository.delete(encontrado);
     }
 }
