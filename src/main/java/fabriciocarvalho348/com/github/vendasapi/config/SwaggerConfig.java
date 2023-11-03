b37e4d679694ad4aabb50d2ea0078a73cc9fa861
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2023-11-03 08:09:40 -0300

    docs: Documentando a API utilizando o SwaggerAPI

---
+++ src/main/java/fabriciocarvalho348/com/github/vendasapi/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package fabriciocarvalho348.com.github.vendasapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("fabriciocarvalho348.com.github.vendasapi.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("Api do projeto de vendas.")
+                .version("0.0.1-SNAPSHOT")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact(
+                "Fabrício de Carvalho",
+                "https://github.com/FabricioCarvalho348/",
+                "fabricioccarvalho348@gmail.com"
+        );
+    }
+}
--- src/main/java/fabriciocarvalho348/com/github/vendasapi/controller/ClienteController.java
+++ src/main/java/fabriciocarvalho348/com/github/vendasapi/controller/ClienteController.java
@@ -1,7 +1,11 @@
 package fabriciocarvalho348.com.github.vendasapi.controller;
 
+import fabriciocarvalho348.com.github.vendasapi.exception.ApiErros;
 import fabriciocarvalho348.com.github.vendasapi.model.entity.Cliente;
 import fabriciocarvalho348.com.github.vendasapi.service.ClienteService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +22,58 @@ public class ClienteController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Obter detalhes de um cliente.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado."),
+            @ApiResponse(code = 404, message = "Cliente não encontrado com o Id informado", response = ApiErros.class)
+    })
     public Cliente pesquisarPorId(@PathVariable Long id) {
         return clienteService.pesquisarPorId(id);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Lista todos os clientes cadastrados.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Lista de clientes cadastrados.")
+    })
     public List<Cliente> pesquisarTodos() {
         return clienteService.pesquisarTodos();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Salva um novo cliente.")
+    @ApiResponses
+            (
+                    {
+                            @ApiResponse(code = 201, message = "Cliente salvo com sucesso."),
+                            @ApiResponse(code = 400, message = "Erro(s) de validação.", response = ApiErros.class)
+                    }
+            )
     public Cliente incluir(@Valid @RequestBody Cliente cliente) {
         return clienteService.incluir(cliente);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Atualiza um cliente existente.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente atualizado com sucesso."),
+            @ApiResponse(code = 400, message = "Erro(s) de validação.", response = ApiErros.class),
+            @ApiResponse(code = 404, message = "Cliente não encontrado.", response = ApiErros.class)
+    })
     public Cliente atualizar(@Valid @RequestBody Cliente cliente, @PathVariable Long id) {
         return clienteService.atualizar(cliente, id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "Exclui um cliente existente.")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Cliente excluído com sucesso."),
+            @ApiResponse(code = 404, message = "Cliente não encontrado.", response = ApiErros.class)
+    })
     public void excluir(@PathVariable Long id) {
         clienteService.excluir(id);
     }
